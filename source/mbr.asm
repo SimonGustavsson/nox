12ac023d85ee05a6bce0ef64abb61b99dd84a52d
Philip Stears <philip@driveworks.co.uk>
2014-10-21 23:22:35 +0100

    fixup tabs

--- source/mbr.asm
+++ source/mbr.asm
@@ -2,7 +2,7 @@
 ; or, 0:0x7C00. We relocate to 0:0x6000 immediately
 
 org 0x6000
-bits 16    				; We're in real mode
+bits 16                 ; We're in real mode
 
 [map all mbr.map]
 
@@ -11,9 +11,9 @@ PARTITION_TABLE_OFFSET_1 EQU PARTITION_TABLE_OFFSET_0 + 0x10
 PARTITION_TABLE_OFFSET_2 EQU PARTITION_TABLE_OFFSET_1 + 0x10
 PARTITION_TABLE_OFFSET_3 EQU PARTITION_TABLE_OFFSET_2 + 0x10
 
-; VBR_OFFSET_BYTES_PER_SECTOR	EQU 0x0B
-VBR_ADDRESS							EQU 0x1000
-VBR_OFFSET_RESERVED_SECTOR_COUNT	EQU 0x0E
+; VBR_OFFSET_BYTES_PER_SECTOR       EQU 0x0B
+VBR_ADDRESS                         EQU 0x1000
+VBR_OFFSET_RESERVED_SECTOR_COUNT    EQU 0x0E
 
 start: 
 
@@ -35,86 +35,86 @@ msg_post db "MBR POS0", 0x0D, 0x0A, 0
 
 ; Block read package tp send  to int13
 readPacket:
-	db 0x10					; Packet size (bytes)
-	db 0 					; Reserved
-	readPacketNumBlocks dw 1 	; Blocks to read
-	readPacketBuffer dw 0x1000	; Buffer to read to
-	dw 0 					; Memory Page
-	readPacketLBA dd 0			; LBA to read from
-	dd 0					; Extra storage for LBAs > 4 bytes
+    db 0x10                         ; Packet size (bytes)
+    db 0                            ; Reserved
+    readPacketNumBlocks dw 1        ; Blocks to read
+    readPacketBuffer dw 0x1000      ; Buffer to read to
+    dw 0                            ; Memory Page
+    readPacketLBA dd 0              ; LBA to read from
+    dd 0                            ; Extra storage for LBAs > 4 bytes
 
 print:
-	lodsb
-	or al, al
-	jz .print_done
-	mov ah, 0eh
-	int 10h
-	jmp print
+    lodsb
+    or al, al
+    jz .print_done
+    mov ah, 0eh
+    int 10h
+    jmp print
 .print_done:
-	ret
+    ret
 
 loader: 
 
 .printPre:
-	xor ax, ax			; ES:SI is the address of the message, clear ES
-	mov es, ax	
-	mov si, msg_pre
-	call print
+    xor ax, ax                      ; ES:SI is the address of the message, clear ES
+    mov es, ax  
+    mov si, msg_pre
+    call print
 
 .tryReset:
-	mov ah, 0		; Reset floppy function
-	int 0x13		; Call
-	jc .tryReset	; Try again if carry flag is set
+    mov ah, 0                       ; Reset floppy function
+    int 0x13                        ; Call
+    jc .tryReset                    ; Try again if carry flag is set
 
 .readVBR:
-	; DL is the drive number, BIOS will have set it to the boot drive,
-	; which in our case ought to be 0x80
+    ; DL is the drive number, BIOS will have set it to the boot drive,
+    ; which in our case ought to be 0x80
 
-	xor bx, bx
-	mov es, bx     ; Zero the destination segment register, offset in BX
+    xor bx, bx
+    mov es, bx                      ; Zero the destination segment register, offset in BX
 
-	; Read the VBR from the first partition
-	; First prepare the package
-	mov word ax, [start + PARTITION_TABLE_OFFSET_0 + 0x8]
-	mov word [readPacketLBA], ax
-	mov word ax, [start + PARTITION_TABLE_OFFSET_0 + 0xA]
-	mov word [readPacketLBA + 2], ax
+    ; Read the VBR from the first partition
+    ; First prepare the package
+    mov word ax, [start + PARTITION_TABLE_OFFSET_0 + 0x8]
+    mov word [readPacketLBA], ax
+    mov word ax, [start + PARTITION_TABLE_OFFSET_0 + 0xA]
+    mov word [readPacketLBA + 2], ax
 
-	; Up to you, big man!
-	mov si, readPacket
-	mov ah, 0x42
-	int 0x13
+    ; Up to you, big man!
+    mov si, readPacket
+    mov ah, 0x42
+    int 0x13
 
-	; The VBR is now loaded at 0x1000
-	mov ax, [VBR_ADDRESS + VBR_OFFSET_RESERVED_SECTOR_COUNT]
+    ; The VBR is now loaded at 0x1000
+    mov ax, [VBR_ADDRESS + VBR_OFFSET_RESERVED_SECTOR_COUNT]
 
-	; TODO: Parse root directory 
+    ; TODO: Parse root directory 
 
-	; TODO: Locate STAGE2.SYS
+    ; TODO: Locate STAGE2.SYS
 
-	; TODO: Load STAGE2.SYS into memory
+    ; TODO: Load STAGE2.SYS into memory
 
-	; TODO: Jump to STAGE2.SYS
+    ; TODO: Jump to STAGE2.SYS
 
 .printVolumeLabel:
-	xor ax, ax			; ES:SI is the address of the message, clear ES
-	mov es, ax	
-	mov si, 0x1000 + 3	; Volume label is 3 bytes into the VBR and is null padded :trollface:
-	call print
+    xor ax, ax          ; ES:SI is the address of the message, clear ES
+    mov es, ax  
+    mov si, 0x1000 + 3  ; Volume label is 3 bytes into the VBR and is null padded :trollface:
+    call print
 
 .printPost:
-	xor ax, ax			; ES:SI is the address of the message, clear ES
-	mov es, ax	
-	mov si, msg_post
-	call print
+    xor ax, ax          ; ES:SI is the address of the message, clear ES
+    mov es, ax  
+    mov si, msg_post
+    call print
 
 .halt:
-	cli
-	hlt
-	; Calculate root directory
+    cli
+    hlt
+    ; Calculate root directory
 
 times 510 - ($-$$) db 0 ; Fill 0's until the 510th byte
-dw 0xAA55 			    ; MBR magic bytes
+dw 0xAA55               ; MBR magic bytes
 
 ; NASM Syntax
 ; vim: ft=nasm expandtab
