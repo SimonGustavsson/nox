43f946447f727cfedc316ad03cfec4b4ab64b8d6
Philip Stears & Simon Gustavsson <philip@philipstears.com>
2015-04-02 18:10:23 +0100

    kernel.h should not have included types

--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -4,10 +4,4 @@
 #define PACKED __attribute__((__packed__))
 #define SECTION_BOOT __attribute__((section(".text.boot")))
 
-#if !defined(__cplusplus)
-#include <stdbool.h> // C Does not have bool :(
-#endif
-
-#include <stdint.h>
-
 #endif
