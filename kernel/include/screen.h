d6d0f5992fe71459c56693a529eb113c76ffb79a
Philip Stears & Simon Gustavsson <philip@philipstears.com>
2015-04-30 20:38:28 +0100

    Adding some pseudo code from skype session with conclusions and some decisions on how to do terminals.

--- kernel/include/screen.h
+++ kernel/include/screen.h
@@ -10,6 +10,29 @@
 //  - Terminal Manager - Manages which terminal is currently active
 //  - Interpreter - reads input and turns it into something the terminal understands
 
+// AWOGA! PSEUDO CODE AHEAD!
+// struct terminal {
+//
+//     // Fur return commands - returns some sort of value
+//     kresult put_strn(char* chars, uint32_t len, void** response),
+//     kresult put_strz(char* chars, void** response);
+//     kresult activate(); // Start rendering
+//     kresult deactive(); // Stop rendering etc...
+//     kresult *receive(key);
+// };
+//
+// struct text_screen {
+//     init();
+//     destroy();
+//    screen_put_char(x,y,c);
+// };
+//
+// struct visual_screen {
+//     init();
+//     destroy();
+//     screen_put_pixel(x,y,p);
+// };
+
 typedef enum
 {
     vga_color_black         = 0,
