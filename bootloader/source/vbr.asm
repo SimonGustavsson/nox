9ddaeb8de64f3381dd1d6794093cc081378d815d
Simon Gustavsson & Philip Stears <simongustavsson@driveworks.co.uk>
2015-06-16 12:56:48 +0100

    Minor VBR rewrite, fat16 and bug fixes

--- bootloader/source/vbr.asm
+++ bootloader/source/vbr.asm
@@ -1,24 +1,27 @@
-section .text
-%include "fat16.inc"
-
 ; ================================================================================
 ; Compile time constants
 ; ================================================================================
 VBR_RUN_ADDRESS            EQU 0x600
 VBR_LOAD_ADDRESS           EQU 0x7C00
 BOOTLOADER_LOAD_ADDRESS    EQU VBR_LOAD_ADDRESS
-
-; Temporary storage for the current FAT sector
-; we're reading whilst following cluster chains
-TEMP_FAT_SECTOR_ADDRESS    EQU (VBR_RUN_ADDRESS + 0x200)
-
+VBR_STACK                  EQU VBR_RUN_ADDRESS
+READ_BUFFER_ADDRESS        EQU (VBR_LOAD_ADDRESS)
 DIR_ENTRY_SIZE             EQU 0x20
+
+; "Variables"
+ROOT_DIR_SECTOR_ADDRESS    EQU (VBR_RUN_ADDRESS       + 0x200)
+FAT0_SECTOR_ADDRESS        EQU (ROOT_DIR_SECTOR_ADDRESS   + 4)
+DATA_SECTOR_START_ADDRESS  EQU (FAT0_SECTOR_ADDRESS       + 4)
+CLUSTER_BYTE_SIZE          EQU (DATA_SECTOR_START_ADDRESS + 4)
+TMP_FAT_BUFFER_ADDRESS     EQU (CLUSTER_BYTE_SIZE         + 4)
 
 ; ================================================================================
 ; NASM Directives
 ; ================================================================================
+section .text
+%include "fat16.inc"
 org VBR_RUN_ADDRESS
-bits 16                  ; We're in real mode
+bits 16                 ; We're in real mode
 [map all build/vbr.map] ; Create map for debugging
 
 ; ================================================================================
@@ -38,177 +41,265 @@ iend
 ; Variables
 ; ================================================================================
 bootloader_name             db "BOOT    SYS" ; DO NOT EDIT, 11 character
-msg_welcome                 db "VBR Loading..."
-bootloader_found            db "BOOT.SYS found", 0x0D, 0x0A, 0
-bootloader_error            db "BOOT.SYS not found", 0x0D, 0x0A, 0
+msg_welcome                 db "NOX", 0x0D, 0x0A, 0
+bootloader_found            db "VBR:)", 0x0D, 0x0A, 0
+bootloader_error            db "VBR:(", 0x0D, 0x0A, 0
 
-; Structure for INT 0x13 (0x42) for reading blocks
-; From the hard drive
+; Structure for INT 0x13 (0x42) for reading blocks from disk
 read_packet:
-                            db 0x10   ; Packet size (bytes)
-                            db 0      ; Reserved
-    read_packet_num_blocks  dw 1      ; Blocks to read
-    read_packet_buffer      dw 0x1000 ; Buffer address
-                            dw 0      ; Memory page
-    read_packet_lba         dd 0      ; LBA to read
-                            dd 0      ; Extra storage for LBAs > 4 bytes
-
-variables:
-    .root_directory_sector  dd 0      ; Sector of root directory
-    .fat0_start_sector      dd 0     ; First sector of FAT0
-    .last_loaded_fat_sector dd 0     ; Last FAT sector loaded whilst following chain
+                            db 0x10                ; Packet size (bytes)
+                            db 0                   ; Reserved
+    read_packet_num_blocks  dw 1                   ; Blocks to read
+    read_packet_buffer      dw READ_BUFFER_ADDRESS ; Buffer address
+                            dw 0                   ; Memory page
+    read_packet_lba         dd 0                   ; LBA to read
+                            dd 0                   ; Extra storage for LBAs > 4 bytes
 
 ; ================================================================================
 ; Start - Relocation
 ; ================================================================================
 code:
 
+    ;
+    ; First of all, the MBR will hand us the LBA of
+    ; the partition we're on in EAX, so that we don't have to
+    ; reach into the partition table ourselves
+    ; (We don't use the values in the BPB for hidden/reserved
+    ; because its a stupid idea to put those values there)
+    ;
+
+    ; The MBR gives us the LBA of the partition,
+    ; However, for FAT calculations, we are only interested in
+    ; where the FAT region starts, so skip past the VBR sector
+    add eax, 1
+    mov [FAT0_SECTOR_ADDRESS], eax; Save the arg we got from the MBR
+
     ; Relocate ourselves so we can load the bootloader to the address
     ; we were loaded to, ensuring the same load address for all stages of the boot
-    mov cx, 0x200                   ; Relocate 512 bytes
     xor ax, ax
+    mov word cx,  0x200                   ; Relocate 512 bytes
     mov es, ax
     mov ds, ax
     mov si, VBR_LOAD_ADDRESS        ; From
     mov di, VBR_RUN_ADDRESS         ; To
     rep movsb
-    
+
     jmp 0:relocated                 ; Far jump to relocated code
 
-    relocated:
+; ================================================================================
+; Relocated code
+; ================================================================================
+relocated:
+    xor ax, ax
+    mov es, ax
+    mov si, msg_welcome
+    call print_int10
 
-        ; We're now at VBR_RUN_ADDRESS
+    mov sp, VBR_STACK           ; Stack setup
 
-        ; Print a message to show relocation worked!
+    .reset_floppy:              ; Prepare for reading
+        mov ah, 0
+        int 0x13
+        jc .reset_floppy
+
+    ;
+    ; FAT Reading code here! Beware! Dun dun dun
+    ;
+    ; ROOT_DIR_SECTOR = (FAT_SIZE * FAT_COUNT) + FAT0_START_SECTOR
+    ; Calculate root directory sector
+    ; (fat_sz * fat_cnt) + fat0_start
+    mov ax, [this_bpb+bpb_fat16.fat_count]       ; Most likely 2
+    mov bx, [this_bpb+bpb_fat16.sectors_per_fat] ; Most likely 32
+    mul bx                                       ; (fat_sz * fat_cnt)
+    add eax, [FAT0_SECTOR_ADDRESS]               ; + fat0_start
+    mov [ROOT_DIR_SECTOR_ADDRESS], eax
+
+    xor ax, ax
+    mov al, [this_bpb + bpb_fat16.sectors_per_cluster]
+    mov [read_packet_num_blocks], ax                  ; We always read full clusters
+
+    ; We need to know the cluster size (in bytes) to increment the buffer
+    ; every time we read a cluster
+    mov bx, [this_bpb + bpb_fat16.bytes_per_sector]
+    mul bx
+    mov [CLUSTER_BYTE_SIZE], bx
+
+    ; Calculate size of root directory
+    mov ax, [this_bpb + bpb_fat16.max_root_entry_count]
+    mov bx, DIR_ENTRY_SIZE
+    mul bx
+
+    ; AX now contains the size of the root directory in bytes
+    ; But how many sectors is that?
+    mov bx, [this_bpb + bpb_fat16.bytes_per_sector]
+    div bx
+
+    ; Save root dir size, we need it after the calculations
+    mov cx, ax
+    add ax, [ROOT_DIR_SECTOR_ADDRESS]
+    mov [DATA_SECTOR_START_ADDRESS], eax
+
+    ; Restore root dir sector size
+    mov ax, cx
+    ; AX now contains the amount of sectors
+    ; the root directory occupies
+    mov [read_packet_num_blocks], ax
+    mov eax, [ROOT_DIR_SECTOR_ADDRESS]
+    call read_int13
+
+    ; The root directory should now be at READ_BUFFER_ADDRESS
+    mov ax, [this_bpb + bpb_fat16.max_root_entry_count]
+    mov word bx, 0x20
+    mul bx
+    mov bx, READ_BUFFER_ADDRESS
+    add ax, bx
+    mov bx, ax
+
+    ; BX now contains the max address of the root directory
+    mov ax, READ_BUFFER_ADDRESS
+
+    .find_kloader:
+        mov si, bootloader_name
+        mov cx, 8 + 3
+        mov di, ax
+        repe cmpsb
+        je .kloader_found
+
+        ; Not this entry, advance to next
+        cmp ax, bx
+        jl .kloader_not_found
+
+        add ax, 0x20
+        jmp short .find_kloader
+
+    .kloader_found:
+
+        mov bx, ax
+        mov cx, [bx + dir_entry.start_cluster]
+        ; Tell the world we found the bootloader
         xor ax, ax
         mov es, ax
-        mov si, msg_welcome
+        mov si, bootloader_found
         call print_int10
-    
-        ; We need to reset the floppy to use int 0x13 to read things
-        .reset_floppy:
-            mov ah, 0
-            int 0x13
-            jc .reset_floppy
 
-        ;
-        ; FAT Reading code here! Beware! Dun dun dun
-        ;
+        ; Load it into memory
+        mov ax, cx
+        call read
 
-        ; First get the sector of the first FAT
-        xor eax, eax
-        mov ax, [this_bpb + bpb_fat16.reserved_sector_count]
+        ; And jump to it!
+        jmp READ_BUFFER_ADDRESS
 
+    .kloader_not_found:
+        xor ax, ax
+        mov es, ax
+        mov si, bootloader_error
+        call print_int10
+
+    halt:
+        cli
+        hlt
+
+;===================================================================================
+; Functions
+;===================================================================================
+; Prints using int 0x10, assumes address to string in ds:si
+print_int10:
+    lodsb
+    or al, al
+    jz .done
+    mov ah, 0xE
+    int 0x10
+    jmp print_int10
+    .done:
+    ret
+
+; In: EAX - LBA to start reading at
+read_int13:
+    mov [read_packet_lba], eax
+    mov si, read_packet
+    mov ah, 0x42
+    mov dl, 0x80
+    int 0x13
+    ret
+
+; Reads the given cluster and all that follows it
+; IN: eax - Cluster to read
+read:
+    ; Reset clusters read so that we read to the start of the buffer
+    mov word [read_packet_buffer], READ_BUFFER_ADDRESS
+
+    .read_loop:
+        ; Before we start trashing registers, save the cluster we just read
+        ; so we can retrive it later to find its FAT entry
+        push eax
+
+        ; Calculate LBA of cluster: first_data_sector + ((n - 2) * sectors_per_cluster)
+        sub eax, 2                                          ; n - 2
         xor ebx, ebx
-        mov ebx, [this_bpb + bpb_fat16.hidden_sector_count]
+        mov bl, [this_bpb + bpb_fat16.sectors_per_cluster]
+        mul ebx                                             ; * sectors_per_cluster
+        add eax, [DATA_SECTOR_START_ADDRESS]                  ; + first_data_sector
+        call read_int13
 
-        add eax, ebx
-        mov [variables.fat0_start_sector], eax
-        ; Calculate root directory sector
-        ; (fat_sz * fat_cnt) + fat0_start
-        xor eax, eax
-        mov ax, [this_bpb+bpb_fat16.fat_count]
+        ; TODO: Int 13h error checking
 
-        xor ebx, ebx
-        mov bx, [this_bpb+bpb_fat16.sectors_per_fat]
+        ; Increment buffer
+        mov ax, [read_packet_buffer]
+        add ax, [CLUSTER_BYTE_SIZE]
+        mov [read_packet_buffer], ax
 
-        mul ebx ; (fat_sz * fat_cnt)
+        ; Get the index of the cluster we just read back out
+        pop eax
 
-        add eax, [variables.fat0_start_sector]
-
-        ; Save this value, we'll need it later
-        mov [variables.root_directory_sector], eax
-
-        ; We now have the variables we need.
-        ; Note: No register state is assumed now
-
-        ; Load the entire root directory into memory
-        ; so that we can find the bootloader!
-
-        ; To load the root directory, we need to know its size
-        xor eax, eax
-        mov ax, [this_bpb + bpb_fat16.max_root_entry_count]
-        xchg bx, bx
-
-        mov ebx, DIR_ENTRY_SIZE
-        mul ebx
-
-        ; eax = byte size of root directory
-        xor ebx, ebx
-        mov bx, [this_bpb + bpb_fat16.bytes_per_sector]
+        ; Get FAT entry for this cluster: FAT0_SECTOR + ((n * 2) / BYTES_PER_SECTOR)
+        mov ecx, 2
+        mul ecx
+        mov ebx, [this_bpb + bpb_fat16.bytes_per_sector]
         div ebx
-        ; TODO: Remainder?
+        mov ebx, [FAT0_SECTOR_ADDRESS]
+        add eax, ebx
+        mov cx, dx
 
-        mov word [read_packet_num_blocks], ax
+        ; EAX now contains the sector to read
+        ;  DX now contains the fat entry offset
 
-        ; The root directory is cluster 2
-        mov eax, 2
-        call get_fat_sector_for_cluster
+        ; Save the read buffer address and read FAT sector to a temp buffer
+        mov bx, [read_packet_buffer]
+        push bx
 
-        call get_fat_entry_for_cluster
+        mov word [read_packet_num_blocks], 1
 
-        .kernel_not_found:
-            xor ax, ax
-            mov es, ax
-            mov si, bootloader_error
-            call print_int10
+        mov bx, TMP_FAT_BUFFER_ADDRESS
+        mov [read_packet_buffer], bx
 
-            hlt
+        call read_int13
 
-        ;
-        ; Functions
-        ;
+        ; TODO: Int 13h error checking
 
-        ; Prints using int 0x10, assumes address to string in ds:si
-        print_int10:
-            lodsb
-            or al, al
-            jz .done
-            mov ah, 0xE
-            int 0x10
-            jmp print_int10
-            .done:
-                ret
-    
-        ; Note: Assumes cluster number in eax
-        ; Returns:
-        ;     EAX: Fat sector to load
-        ;      DX: Entry offset in sector
-        get_fat_sector_for_cluster:
+        ; Restore file read buffer
+        pop ax
+        mov [read_packet_buffer], ax
 
-            ; Calculate FAT offset
-            mov ecx, 2
-            mul ecx
-            mov ebx, [this_bpb + bpb_fat16.bytes_per_sector]
-            div ebx
+        ; Get address to entry
+        xor eax, eax
+        add cx, TMP_FAT_BUFFER_ADDRESS ; Add base address to entry offset
+        mov ax, cx
+        mov word ax, [eax]
 
-            mov ebx, [variables.fat0_start_sector]
+        ; Restore sectors to read
+        mov bl, [this_bpb + bpb_fat16.sectors_per_cluster]
+        mov [read_packet_num_blocks], bl                  ; We always read full clusters
 
-            add eax, ebx
-            ; EAX = FAT sector to load now
-            ; DX  = FAT offset
-            ret
+        ; EAX should now contain the FAT entry
+        cmp ax, 0xFFF7
 
-        ; Note: assumes cluster number in EAX
-        ; Note: assumes entry offset in sector in DX
-        get_fat_entry_for_cluster:
-            mov [read_packet_lba], eax
+        ; Buffer has already been all set up so we're ready
+        ; to read the next cluster
+        jl .read_loop
 
-            mov ecx, TEMP_FAT_SECTOR_ADDRESS
-            mov [read_packet_buffer], ecx
-            ; Load in the sector
-            ; TODO: Check if it's already loaded (variable for it)
-            mov si, read_packet
-            mov ah, 0x42
-            mov dl, 0x80 ; TODO: Trashing argument here!
-            int 0x13
+    .done:
+        ret
 
-            mov eax, 0x7; TODO: Eax = TEMP_FAT_SECTOR_ADDRESS + dx
-
-            ret
-        halt:
-            cli
-            hlt
 ; ================================================================================
 ; Padding & Signature
 ; ================================================================================
