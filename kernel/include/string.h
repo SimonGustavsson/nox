8a1e796b28b5387c6f7abb900f5fd419b7c44a20
Simon Gustavsson & Philip Stears <simongustavsson@driveworks.co.uk>
2015-06-17 10:27:58 +0100

    itoa now returns a ptr to the buffer

--- kernel/include/string.h
+++ kernel/include/string.h
@@ -4,7 +4,7 @@
 #include <types.h>
 
 uint8_t nybble_to_ascii(uint8_t val);
-void itoa(int number, char* buf);
+char* itoa(int32_t number, char* buf);
 size_t strlen(const char* str);
 bool kstrcmp(char* a, char* b);
 
