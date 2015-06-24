991faaa94b1a00bc53cf915f152db14351cef34a
Philip Stears & Simon Gustavsson <philip@philipstears.com>
2015-06-24 19:50:32 +0100

    Pushing because no electricity so switching to Simon's machine :'(

--- kernel/source/ata.c
+++ kernel/source/ata.c
@@ -24,6 +24,28 @@ void ata_write(enum ata_controller controller, enum ata_register port, uint8_t v
     OUTB(controller + port, value);
 }
 
+// Note: a block count of 0 = 256 sectors
+void ata_read_sectors(uint32_t lba, uint8_t block_count, uintptr_t* buffer)
+{
+// An example of a 28 bit LBA PIO mode read on the Primary bus:
+//
+//  Send 0xE0 for the "master" or 0xF0 for the "slave", ORed with the highest 4 bits of the LBA to port 0x1F6:
+    enum ata_drive drive = ata_drive_master;
+    // Structure of the drive_head register as it pertains to LBA is
+    // 7   6   5   4    |    3  2   1   0
+    // 1  LBA  1 Drive  | High 4 Bits of LBA
+    ata_write(ata_controller_primary, ata_register_drive_head, 0xE0 | (drive << 4) | ((lba >> 24) & 0x0F));
+//  Send a NULL byte to port 0x1F1, if you like (it is ignored and wastes lots of CPU time): outb(0x1F1, 0x00)
+//  Send the sectorcount to port 0x1F2: outb(0x1F2, (unsigned char) count)
+//  Send the low 8 bits of the LBA to port 0x1F3: outb(0x1F3, (unsigned char) LBA))
+//  Send the next 8 bits of the LBA to port 0x1F4: outb(0x1F4, (unsigned char)(LBA >> 8))
+//  Send the next 8 bits of the LBA to port 0x1F5: outb(0x1F5, (unsigned char)(LBA >> 16))
+//  Send the "READ SECTORS" command (0x20) to port 0x1F7: outb(0x1F7, 0x20)
+//  Wait for an IRQ or poll.
+//  Transfer 256 16-bit values, a uint16_t at a time, into your buffer from I/O port 0x1F0. (In assembler, REP INSW works well for this.)
+//  Then loop back to waiting for the next IRQ (or poll again -- see next note) for each successive sector.
+}
+
 void ata_init() {
     select_drive(ata_controller_primary, ata_drive_master);
 }
