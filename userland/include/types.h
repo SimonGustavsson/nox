bd403ca219faeae8a7e480df62de7a833c70a4fc
Simon Gustavsson & Philip Stears <simongustavsson@driveworks.co.uk>
2015-07-04 12:31:27 +0100

    Userland now prints hello in the top left corner!

---
+++ userland/include/types.h
@@ -0,0 +1,24 @@
+#ifndef NOX_TYPES_H
+#define NOX_TYPES_H
+
+#include <stdbool.h> // C Does not have bool :(
+#include <stdint.h>
+
+#if PLATFORM_BITS==32
+typedef uint32_t            native_uint_t;
+typedef int32_t             native_int_t;
+#elif PLATFORM_BITS==64
+typedef uint64_t            native_uint_t;
+typedef int64_t             native_int_t;
+#else
+#error  PLATFORM_BITS has an invalid value
+#endif
+
+typedef native_uint_t       size_t;
+
+#define NULL                (0)
+#define UINT64_MAXVALUE 18446744073709551615
+#define UINT32_MAXVALUE 4294967295
+#define UINT16_MAXVALUE 65535
+
+#endif
